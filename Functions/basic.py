ac86fb7e02c3a1a0987b4ac31712cb9aa70f51e1
Mahesh Patidar <maheshpatidar3550@gmail.com>
2026-05-25 18:01:00 +0530

    New code addea

---
+++ Functions/basic.py
@@ -0,0 +1,75 @@
+# def message():
+#     print("Enter a value: ")
+
+# message()
+# a = int(input())
+# message()
+# b = int(input())
+# message()
+# c = int(input())
+
+# print(message)
+
+# def message():
+#     print("Enter a value: ")
+#     num = int(input())
+#     return num
+# a = message()
+# b = message()
+# print("a: ",a,"b: ",b)
+
+# PARAMETERIZED FUNCTION
+
+# def hello(n):
+#     print("Hello,",n)
+# name = input("Enter your name: ")
+# hello(name)
+
+# def fun(num):
+#     print("Number: ",num)
+# num = 457
+# fun(2)
+# print(num)
+
+# def message(what,number):
+#     print("Enter",what,"number: ",number)
+# message("roll",101)
+
+# def intro(first_name,last_name):
+#     print("Hello, my name is",first_name,last_name)
+# intro("Peter","Parkar")
+# intro(first_name="Aditya",last_name="Gupta")
+
+# def add(a,b,c):
+#     print(a, "+", b, "+", c, "=",a+b+c)
+# add(1,2,4)
+# add(c=1,a=2,b=4)
+# add(1,c=2,b=4)
+# #add(1,a = 2,b = 4)  ERROR
+
+# def new_year(wishes = True):
+#     print("Three...")
+#     print('Two...')
+#     print("One...")
+#     if not wishes:
+#         return
+#     print("Happy new year!")
+# new_year(True)
+# new_year(False)
+
+# def boring_fun():
+#     print("Boring")
+#     return 123
+# print("This lesson is")
+# boring_fun()
+
+def checkMyVar(var):
+    if(var == 10):
+        print("Variable is 10")
+        return 1
+    else:
+        print("Variable is not up to the mark")
+        return
+checkMyVar(10)
+checkMyVar(5)
+print(checkMyVar(5))  
---
+++ Functions/listOperation.py
@@ -0,0 +1,14 @@
+# def list_sum(lst):
+#     s = 0
+#     for i in lst:
+#         s += i
+#     return s
+# print(list_sum([4,2,2]))
+
+def list_fun(n):
+    list =[]
+    for i in range(0,n):
+        list.insert(0,i)
+        #list.append(i+1)
+        return list_fun
+print(list_fun(6))

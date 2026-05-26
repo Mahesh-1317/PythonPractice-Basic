75910a4dd650d2517625b3562f0e6327dde64cd4
Mahesh Patidar <maheshpatidar3550@gmail.com>
2026-05-26 12:41:53 +0530

    Assignment1 added

--- Assignment1/tempCodeRunnerFile.py
+++ Assignment1/tempCodeRunnerFile.py
@@ -1,8 +1,7 @@
-
-# for i in range(1,n+1):
-#     for j in range(i,2*n):
-#         if j == n+i-2 or j == n+i-5 or i == n:
-#             print("*",end="")
-#         else:
-#             print(" ",end=" ")
-#     print()
+for i in range(1,n+1):
+    for j in range (1,2*n):
+        if (j == n-i+1 or j == n+i-1):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()

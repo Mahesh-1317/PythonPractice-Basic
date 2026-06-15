652f743c8b05c75ce53912d8383c4cb1eb8e908c
Mahesh Patidar <maheshpatidar3550@gmail.com>
2026-06-15 18:15:27 +0530

    Pandas with csv

--- Libraries/Pandas/students_data.py
+++ Libraries/Pandas/students_data.py
@@ -1,10 +1,11 @@
 import pandas as pd
+import csv
 
 data = {
-    'Name': ['Pankaj','Alia','Ram','Kajal','Aman'],
+    'Name': ['Dr Octopus','Vecna','Dr Octopus','Sambha','Vecna'],
     'Age': [21,19,22,20,19],
     'Marks': [85,65,75,99,56],
-    'City': ['Rom','Berlin','Tokyo','Moscow','London']
+    'City': ['Rom','Berlin','Tokyo','Berlin','Rom']
 }
 
 df = pd.DataFrame(data)
@@ -14,3 +15,43 @@ print(df.shape)
 print(df.head(2))
 print(df.dtypes)
 print(df.describe)      # Statisticsl summary
+
+print()
+#   Select column
+print("df['Name']: \n", df['Name'])
+print(df[['Name', 'Marks']])        #   Multiple DataFrames
+
+print()
+#   Filter rows
+print(df[df['Marks'] >= 85])
+print(df[df['City'] == 'Berlin'])
+
+print()
+print(df[ (df['Marks'] >= 75) & (df['City'] == 'Tokyo')])
+
+def get_grade(x):
+    if x >= 90:
+        return 'A'
+    elif x >= 70:
+        return 'B'
+    else:
+        return 'C'
+    
+df['Grade'] = df['Marks'].apply(get_grade)
+print(df['Grade'])
+print()
+print(df)
+
+#   Groupby
+city_avg = df.groupby('City')['Marks'].mean()
+age_avg = df.groupby('Name')['Age'].min()
+print(city_avg)
+print()
+print(age_avg)
+
+#   Read csv file
+print()
+
+df.to_csv('Libraries/Pandas/students.csv',index=False)
+df2 = pd.read_csv("Libraries/Pandas/students.csv")
+df2.to_csv('Libraries/Pandas/clean_output.csv',index=False)

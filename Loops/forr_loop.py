51daa60b0662de230c262fc911c4803f5fcf7bfd
Mahesh Patidar <maheshpatidar3550@gmail.com>
2026-06-17 16:16:00 +0530

    padding and loop

---
+++ Loops/forr_loop.py
@@ -0,0 +1,5 @@
+names = ["Mahesh", "Rohit", "Aman"]
+marks = [80, 90, 70]
+
+for name, mark in zip(names, marks):
+    print(name, mark)
---
+++ id_card.py
@@ -0,0 +1,6 @@
+name,age,city,state,skill = 'Vijay Dinanath Chauhan', 22, 'Los Angeles', 'California','Python & Machine Learning'
+
+print(f'NAME    :{name}')
+print(f'AGE     :{age}')
+print(f'CITY    :{city}, {state}')
+print(f'SKILL   :{skill}')
